d7b901abce8bc769c94e0ce14a31defb3c208121
L Redkin <golfcreatr@gmail.com>
2025-04-22 11:52:23 +0300

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import CharContainer from "./components/CharContainer";
 import HeroSection from "./components/HeroSection";
-
+import FeedBack from "./components/FeedBack";
 function App() {
   return (
     <>
       <div className="w-full min-h-screen bg-gradient-to-br from-purple-900 via-green-900 to-black bg-[length:200%_200%] animate-[gradient_15s_ease_infinite] py-4">
         <HeroSection />
         <CharContainer />
+        <FeedBack />
       </div>
     </>
   );
